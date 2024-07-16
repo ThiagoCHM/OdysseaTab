f605c878b07661a4b5a4c98332c7128fb0966e58
Thiago C H Moreira <thiagochm@gmail.com>
2024-07-16 17:27:22 -0300

    Criação de Base para Mobile

---
+++ mobile/src/components/loading.tsx
@@ -0,0 +1,7 @@
+import { ActivityIndicator } from "react-native"
+
+export function Loading() {
+  return (
+    <ActivityIndicator className="flex-1 bg-slate-950 items-center justify-center text-sky-500" />
+  )
+}
